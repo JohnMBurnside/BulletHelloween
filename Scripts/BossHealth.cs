3988da91b44d756c86d70fe3cc5dbe5335b2b3b4
JohnMBurnside <51937396+JohnMBurnside@users.noreply.github.com>
2019-10-25 12:51:02 -0700

    Updated boss health script

--- Scripts/BossHealth.cs
+++ Scripts/BossHealth.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+uusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,33 +6,39 @@ public class BossHealth : MonoBehaviour
 {
 	//VARIABLES
 	public int bossHealth = 50;
-	public Slider bossSlider = bossHealth;
-	//START FUNCTION
-	void Start()
+    public Slider bossSlider;
+    ///public GameObject bossHUD;
+    //START FUNCTION
+    void Start()
 	{
-		GetComponent<canvas>().enabled = false;
+        ///bossHUD.GetComponent<Canvas>().enabled = false;
 		bossSlider.maxValue = bossHealth;
-		bossSlider.Value = bossHealth;
+		bossSlider.value = bossHealth;
 	}
 	//UPDATE FUNCTION
 	void Update()
 	{
 		if(bossHealth < 1)
-			Destroy(gameObject);
-	}
-	//TRIGGER FUNCTION
-	void OnTriggerEnter2D(Collision2D collision)
-	{
-		if(collision.gameObject.tag == "Player")
-			GetComponent<canvas>().enabled = true;
-	}
-	//COLLISION FUNCTION
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if(collision.gameObject.tag == "Bullet")
-		{
-			bossHealth--;
-			bossSlider.Value = bossHealth;
-		}
-	}
+        {
+            Destroy(gameObject);
+            ///bossHUD.GetComponent<Canvas>().enabled = false;
+        }
+    }
+    //TRIGGER FUNCTION
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        ///if (collision.gameObject.tag == "Player")
+            ///bossHUD.GetComponent<Canvas>().enabled = true;
+        ///else if (collision.gameObject.tag == "Player")
+            ///bossHUD.GetComponent<Canvas>().enabled = false;
+    }
+    //COLLISION FUNCTION
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            bossHealth--;
+            bossSlider.value = bossHealth;
+        }
+    }
 }
